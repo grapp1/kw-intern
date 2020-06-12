10fc9616ce5fd538c007afbdd9ae78ed76726810
Garrett Rapp <garrett.rapp@kitware.com>
2020-06-12 12:48:24 -0400

    added param_reader script

--- parflow/tcl-python/examples/washita/param_reader.py
+++ parflow/tcl-python/examples/washita/param_reader.py
@@ -4,18 +4,26 @@
 
 import pandas as pd
 
+# initializing the lists and output dictionary
 param_keys = []
 param_vals = []
+params = {}
 
-pd.read_csv
 
+# setting up to read csv and convert to key/value format: e.g. Geom.domain.Perm.Value
 with open('LW_params.csv') as csv_file:
     LW_params = pd.read_csv(csv_file)
-    print(LW_params)
     
+    # completing list of keys and values from csv file
     for key, value in LW_params.iteritems(): 
-        print(key,".",value)
-        print() 
-                    
-                    
+        param_keys.append(key)
+        param_vals.append(value)
+    
+    # converting lists to a dictionary
+    for i in range(1,len(param_keys)):
+        for j in range(len(param_vals[1])):
+            params[f'Geom.{param_vals[0][j]}.{param_keys[i]}.Value'] = \
+            param_vals[i][j]
+            
+print(params)                  
 
