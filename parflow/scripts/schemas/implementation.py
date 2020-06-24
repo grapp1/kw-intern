7c3b9bc85bdb804421b13d8b58e3df82d126d06d
Garrett Rapp <grapp@email.arizona.edu>
2020-06-24 18:13:45 -0400

    trying to fix autocomplete

--- parflow/scripts/schemas/implementation.py
+++ parflow/scripts/schemas/implementation.py
@@ -1,7 +1,16 @@
 # implementation.py
 
 from schema_v2 import parflow as pf
+from schema_v2 import PFDict
 
 test = pf('test')
 print(test.runname)
+print(test.pf['Process']['Topology']['R'])
+test.pf['Process']['Topology']['R'] = 5
+print(test.pf['Process']['Topology']['R'])
+
+a = {'root': 3, 'parent': {'child': 2}}
+pf_dict = PFDict(a)
+print(pf_dict)
+print(pf_dict['root'])
 
--- parflow/scripts/schemas/schema_v2.py
+++ parflow/scripts/schemas/schema_v2.py
@@ -10,3 +10,29 @@ class parflow:
         self.runname = runname
         with open(config_file) as file:
             self.pf = yaml.load(file, Loader=yaml.FullLoader)
+
+
+class PFDict(dict):
+  def __init__(self, d):
+    super().__init__(d)
+    self.convert_children()
+
+  def convert_children(self):
+    def convert_recursive(child):
+      for key, value in child.items():
+        if isinstance(value, dict):
+          child[key] = PFDict(value)
+          convert_recursive(child[key])
+
+    convert_recursive(self)
+
+  def __getattr__(self, item):
+    if item in self.d:
+      return self.d[item]
+
+  def __dir__(self):
+    return self.d.keys()
+
+
+
+
