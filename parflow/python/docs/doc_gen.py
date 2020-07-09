f0af50aa0cb6de267d9079ceacc738575fdda9a1
Garrett Rapp <grapp@email.arizona.edu>
2020-07-09 19:16:53 -0400

    documentation working

--- parflow/python/docs/doc_gen.py
+++ parflow/python/docs/doc_gen.py
@@ -1,11 +1,42 @@
-# Running tests to generate documentation from yaml/json files
+# Generator to convert yaml files to markdown
 
-import pypandoc as ppd
-import json
+import yaml
+
+'''
+Example:
+
+YAML:
+
+Process:
+  __doc__: >
+    run.Process input options are: Topology
+
+  Topology:
+    __doc__: >
+      [Type: int] Documentation
+
+    P:
+      help: >
+        [Type: int] P allocates the number of processes to the grid-cells in x.
+      default: 1
+      domains:
+        IntValue:
+          minValue: 1
 
 
-with open('demo1_lw_test.json') as json_file:
-    data = json.load(json_file)
+Markdown:
 
+## Process.Topology.P
+*Integer* **[default: 1]**  This allocates the number of processes to the grid-cells in x.
+'''
 
-output = ppd.convert_text(json.dumps(data), format='json',to='md', outputfile='test.md')
+with open(r'../parflow/database/definitions/core.yaml') as file:
+  core = yaml.load(file, Loader=yaml.FullLoader)
+
+not_keys=['__doc__', 'help', 'domains', 'default']
+
+def keyBuilder:
+  for key, value in core.items():
+    if value not in not_keys:
+      print(value)
+
