7e3e60c2b37aaf667381b0a93f4038acc93a49f8
Garrett Rapp <grapp@email.arizona.edu>
2020-08-04 16:17:57 -0700

    updated batch testing

--- parflow/python/test/batch_test.py
+++ parflow/python/test/batch_test.py
@@ -1,10 +1,13 @@
-import runpy
+import runpy, os
 
 
 test_files = ['default_richards.py',
               'water_balance_x.py',
               'default_single.py']
 
+os.chdir('./examples')
+
 for file in test_files:
-  runpy.run_path(path_name=f'./test/{file}')
+  runpy.run_path(path_name=f'./{file[:-3]}/{file}')
+  os.chdir('../')
 
