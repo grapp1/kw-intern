30fefbca1cfa5adfdc6834eabfebe076d850bc45
Garrett Rapp <grapp@email.arizona.edu>
2020-08-14 16:57:56 -0700

    working on pypi packaging

--- parflow-integration/setup.py
+++ parflow-integration/setup.py
@@ -1,5 +1,5 @@
 import pathlib
-from setuptools import setup
+from setuptools import find_packages, setup
 
 # reading the README file
 HERE = pathlib.Path(__file__).parent
@@ -10,15 +10,16 @@ setup(
     version="1.0.0",
     description='A package to run ParFlow via a Python interface.',
     long_description=README,
+    long_description_content_type='text/markdown',
     url='https://github.com/grapp1/kw-intern/tree/master/parflow-integration',
-    author='Kitware Inc',
+    author='Kitware, Inc.',
     license='###',
     classifiers=[
         'Programming Language :: Python :: 3',
     ],
     keywords='',
-    packages=['parflow'],
-    package_dir={'':'pftools/python'},
+    packages=find_packages(),
+    # package_dir={'':'pftools/python'},
     install_requires=[
         'pyyaml',
         'sphinx_rtd_theme'
